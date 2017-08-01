542e2b51f881bdae427d8b76ca7230657a3a1d33
Paul <ruizalba.007@gmail.com>
2017-08-01 18:02:22 +0200

    Division on intelligible and sensible (our maze) realm, attempt for completion of intelligible realm

---
+++ intelligiblerealm/fcn.c
@@ -0,0 +1,25 @@
+/*main function
+	Evaluates agent decisions, maze answers and if victory has been achieved.
+	Makes the game evolute.
+*/
+
+int main(){
+
+	bool defeat=true;//Schopenhauer: life is a defeat, until we get what we want. Then, we will desire another thing, and we will thus be in defeat again. Life is a defeat.
+
+	while(defeat)
+	{
+		
+		agent.decides(); //decision algorithm for the current environment
+		agent.acts(); //agent acts (i.e. moves to an eligible position)
+		if (victory()) //victory evaluates condition to win
+			defeat=false;
+		else
+			maze.updates(); //maze updates, case of changing maze with time (ennemies spawn, moving walls...)
+		
+		
+		
+	}
+	return 0;
+}
+	
