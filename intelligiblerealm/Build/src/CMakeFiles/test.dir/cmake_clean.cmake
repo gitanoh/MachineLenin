99e32ca93a1f0ebbffe94a3c0a0483e8b8387c72
Quan <minh_quan_95@gyahoo.com>
2017-08-03 22:56:25 +0200

    my cpp test w.i.p

---
+++ intelligiblerealm/Build/src/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/Main.obj"
+  "CMakeFiles/test.dir/cmaze/Maze.obj"
+  "test.pdb"
+  "test.exe"
+  "test.exe.manifest"
+  "libtest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
